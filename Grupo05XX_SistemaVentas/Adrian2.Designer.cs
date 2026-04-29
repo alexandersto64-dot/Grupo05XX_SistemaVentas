54fba254e98ce402d99683456e15f24a69b29c32
07CHMK1PA111A08 <07CHMK1PA111A08@07CHMK1PA111A08>
2026-04-29 10:34:10 -0500

    3.cambio a la hora de ejecutar

--- Grupo05XX_SistemaVentas/Adrian2.Designer.cs
+++ Grupo05XX_SistemaVentas/Adrian2.Designer.cs
@@ -412,7 +412,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(774, 40);
+            this.button1.Location = new System.Drawing.Point(753, 35);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(115, 37);
             this.button1.TabIndex = 55;
