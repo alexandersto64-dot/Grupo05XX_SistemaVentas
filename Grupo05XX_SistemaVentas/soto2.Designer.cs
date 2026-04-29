67c0e1074f217f5578d19b63f5bff3c299bbe389
Alejandro <alejandroeufracio256@gmail.com>
2026-04-29 10:24:55 -0500

    2.Actualizacion de Formulario

--- Grupo05XX_SistemaVentas/soto2.Designer.cs
+++ Grupo05XX_SistemaVentas/soto2.Designer.cs
@@ -124,7 +124,7 @@
             // 
             this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(100, 189);
+            this.button1.Location = new System.Drawing.Point(100, 206);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(133, 59);
